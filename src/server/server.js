b9882e3a5268ed5277a565691cab3afa09b1207f
Lucas Caballero <caballerolucas18@gmail.com>
2026-01-22 15:27:40 -0300

    pude lograr cambiando el asterisco por el any

--- src/server/server.js
+++ src/server/server.js
@@ -10,7 +10,7 @@ const startServer = (options) =>{
     // para poder usar los middlewere se usa la palabra use (express)
     app.use(express.static(public_path)) // contenido estatico que ponemos disponible
     
-    app.get ('#' , (req, res) =>{
+    app.get ('/:any' , (req, res) =>{
         const indexPath = path.join(__dirname + `../../../${public_path}/index.html`)
         res.sendFile(indexPath)
     } )
